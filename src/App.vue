84fabcd82344fab2f8266d5caae24560871da5a5
FranzBurneo <franzdavidburneo@gmail.com>
2023-07-29 23:30:30 -0500

    feat: buttons to test vue fundaments

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ var name = "Franz Burneo";
 
 const counter = ref(0);
 const styleColor = ref('color: white');
+const listaNumeros = ref([]);
 
 const increment = () => {
   counter.value++;
@@ -14,18 +15,28 @@ const decrease = () => {
 const reset = () => {
   counter.value = 0;
 }
-
 const classCounter = computed(() => {
   return counter.value > 0 ? 'positive' :
-                            counter.value < 0 ? 'negative' : 'zero';
+                            counter.value < 0 ? 'negative' :
+                             'zero';
 })
+
+const added = computed(() => {
+  return listaNumeros.value.includes(counter.value) ? true : false;
+})
+
+const add = () =>{
+  listaNumeros.value.push(counter.value);
+}
 </script>
 <template>
   <h1> Hola {{ name.toUpperCase() }}</h1>
   <h2 :class="classCounter">{{ counter }}</h2>
-  <button @click="increment">Incrementar</button>
-  <button @click="decrease">Disminuir</button>
-  <button @click="reset">Resetear</button>
+  <button @click="increment" class="btn btn-success">Incrementar</button>
+  <button @click="decrease" class="btn btn-danger">Disminuir</button>
+  <button @click="reset" class="btn btn-primary">Resetear</button>
+  <button @click="add" class="btn btn-warning" :disabled="added">Add</button>
+  <h2 >{{ listaNumeros }}</h2>
 </template>
 
 
